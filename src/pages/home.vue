35f8db3f83090d4e7e4def39359b5b4b3d72ecaa
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-04 21:30:59 +0700

    firstName dont active in CompositinAPI

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,4 +1,24 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <h1>Overview page</h1>
+  <h1>{{ firstName }}</h1>
+  <button v-on:click="onChangeSomething()">Click here to show log</button>
 </template>
+
+<script>
+export default {
+  setup() {
+    let firstName = "q";
+
+    function onChangeSomething() {
+      // Sẽ không thay đổi firstName, vì ko có reactive như bên hàm data() cũ, vậy phải làm sao.?
+      firstName = "new Name";
+      console.log("hee...");
+    }
+    return {
+      firstName,
+      onChangeSomething,
+    };
+  },
+};
+</script>
