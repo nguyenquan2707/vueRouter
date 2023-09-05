885748568022084524dd494ba53233c65b7d2552
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-05 19:57:01 +0700

    computed but not success

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,55 +1,28 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <h1>Overview page</h1>
-  <h1>{{ firstName }}</h1>
-  <h1>{{ secondName }}</h1>
-  <button v-on:click="onChangeSomething()">Click here to show log</button>
-
-  <p>{{ car }}</p>
+  <input type="text" v-model="searchName" />
+  <ul>
+    <li v-for="(employee, index) in employees" :key="index">
+      {{ employee }}
+    </li>
+  </ul>
 </template>
 
 <script>
 //ref() reactive reference.
 //Có nhiều chức năng từ vue, khai báo như vậy là dùng chỉ thằng ref.
-import { ref, reactive } from "vue";
+import { computed, ref, reactive } from "vue";
 export default {
   setup() {
-    //Ref<String> firstName;
-    let firstName = ref("q");
+    const searchName = ref("");
+    const employees = reactive(["quan", "duc", "kien"]);
 
-    //secondName là Proxy Object of RefImpl
-    let secondName = ref({
-      name: "Tra",
-      middleName: "Thanh",
-    });
+    const searchEmployees = computed(() =>
+      employees.filter((employee) => employee.includes(searchName.value))
+    );
 
-    //car là Proxy object, vì vậy có thể truy cập nó trực tiếp mà
-    // ko cần phải thông qua value như ref().
-    let car = reactive({
-      price: 100,
-      name: "BMW",
-    });
-
-    function onChangeSomething() {
-      // Sẽ không thay đổi firstName, vì ko có reactive như bên hàm data() cũ, vậy phải làm sao.?
-      console.log(firstName); //https://ibb.co/VTcfpq4
-      firstName.value = "new Name";
-
-      car.name = "Winfast";
-
-      console.log(secondName);
-      //change secondName by new objects
-      secondName.value = {
-        name: "Tra New",
-        address: "Cam ranh",
-      };
-    }
-    return {
-      firstName,
-      secondName,
-      onChangeSomething,
-      car,
-    };
+    return { searchName, searchEmployees, employees };
   },
 };
 </script>
